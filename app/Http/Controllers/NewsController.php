b2cc3368ffcbc5f31462d4c0fdc127d70c584bfe
AntoineWeis <wantoine76@gmail.com>
2018-07-13 14:48:11 +0200

    -Inserting yield -templating the header part -templating the breadcrumbs -templating the content

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -8,7 +8,7 @@ class NewsController extends Controller
 {
     public function index()
     {
-        return view('abricot/main');
+        return view('abricot/news/index');
     }
 
 
---
+++ resources/views/abricot/news/index.blade.php
@@ -0,0 +1,330 @@
+@extends('abricot/main')
+
+
+@section('header')
+<aside id="colorlib-hero">
+    <div class="flexslider">
+        <ul class="slides">
+        <li style="background-image: url(img/abricot/img_bg_1.jpg);">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-md-pull-3 col-sm-12 col-xs-12 col-md-offset-3 slider-text">
+                        <div class="slider-text-inner">
+                            <div class="desc">
+                                <p class="meta">
+                                        <span class="cat"><a href="#">Events</a></span>
+                                        <span class="date">20 March 2018</span>
+                                        <span class="pos">By <a href="#">Rich</a></span>
+                                    </p>
+                                <h1>How a website designer began customizing and designing</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li style="background-image: url(img/abricot/img_bg_2.jpg);">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-md-pull-3 col-sm-12 col-xs-12 col-md-offset-3 slider-text">
+                        <div class="slider-text-inner">
+                            <div class="desc">
+                                <p class="meta">
+                                        <span class="cat"><a href="#">Events</a></span>
+                                        <span class="date">20 March 2018</span>
+                                        <span class="pos">By <a href="#">Rich</a></span>
+                                    </p>
+                                <h1>How a website designer began customizing and designing</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li style="background-image: url(img/abricot/img_bg_3.jpg);">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-md-pull-3 col-sm-12 col-xs-12 col-md-offset-3 slider-text">
+                        <div class="slider-text-inner">
+                            <div class="desc">
+                                <p class="meta">
+                                        <span class="cat"><a href="#">Events</a></span>
+                                        <span class="date">20 March 2018</span>
+                                        <span class="pos">By <a href="#">Rich</a></span>
+                                    </p>
+                                <h1>How a website designer began customizing and designing</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li style="background-image: url(img/abricot/img_bg_4.jpg);">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-md-pull-3 col-sm-12 col-xs-12 col-md-offset-3 slider-text">
+                        <div class="slider-text-inner">
+                            <div class="desc">
+                                <p class="meta">
+                                        <span class="cat"><a href="#">Events</a></span>
+                                        <span class="date">20 March 2018</span>
+                                        <span class="pos">By <a href="#">Rich</a></span>
+                                    </p>
+                                <h1>Top 5 Places To Start Your Vacation This Summer</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </li>
+        </ul>
+    </div>
+</aside>
+@stop
+
+@section('content')
+<div class="row row-pb-md">
+    <div class="col-md-4">
+        <div class="blog-entry">
+            <div class="blog-img">
+                <a href="blog.html"><img src="img/abricot/blog-1.jpg" class="img-responsive" alt="html5 bootstrap template"></a>
+            </div>
+            <div class="desc">
+                <p class="meta">
+                    <span class="cat"><a href="#">Events</a></span>
+                    <span class="date">20 March 2018</span>
+                    <span class="pos">By <a href="#">Rich</a></span>
+                </p>
+                <h2><a href="blog.html">Recipe for your site</a></h2>
+                <p>A small river named Duden flows by their place and supplies it with the necessary </p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="blog-entry">
+            <div class="blog-img">
+                <a href="blog.html"><img src="img/abricot/blog-2.jpg" class="img-responsive" alt="html5 bootstrap template"></a>
+            </div>
+            <div class="desc">
+                <p class="meta">
+                    <span class="cat"><a href="#">Read</a></span>
+                    <span class="date">20 March 2018</span>
+                    <span class="pos">By <a href="#">Rich</a></span>
+                </p>
+                <h2><a href="blog.html">Recipe for your site</a></h2>
+                <p>A small river named Duden flows by their place and supplies it with the necessary </p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="blog-entry">
+            <div class="blog-slider">
+                <div class="owl-carousel">
+                    <div class="item">
+                        <a href="blog.html" class="image-popup-link"><img src="img/abricot/blog-3.jpg" class="img-responsive" alt="html5 bootstrap template"></a>
+                    </div>
+                    <div class="item">
+                        <a href="blog.html" class="image-popup-link"><img src="img/abricot/blog-4.jpg" class="img-responsive" alt="html5 bootstrap template"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="desc">
+                <p class="meta">
+                    <span class="cat"><a href="#">Travel</a></span>
+                    <span class="date">20 March 2018</span>
+                    <span class="pos">By <a href="#">Rich</a></span>
+                </p>
+                <h2><a href="blog.html">Recipe for your site</a></h2>
+                <p>A small river named Duden flows by their place and supplies it with the necessary </p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row row-pb-md">
+    <div class="col-md-4">
+        <div class="blog-entry">
+            <div class="blog-img">
+                <a href="blog.html"><img src="img/abricot/blog-4.jpg" class="img-responsive" alt="html5 bootstrap template"></a>
+            </div>
+            <div class="desc">
+                <p class="meta">
+                    <span class="cat"><a href="#">Lifestyle</a></span>
+                    <span class="date">20 March 2018</span>
+                    <span class="pos">By <a href="#">Rich</a></span>
+                </p>
+                <h2><a href="blog.html">Recipe for your site</a></h2>
+                <p>A small river named Duden flows by their place and supplies it with the necessary </p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="blog-entry">
+            <div class="blog-img">
+                <a href="blog.html"><img src="img/abricot/blog-5.jpg" class="img-responsive" alt="html5 bootstrap template"></a>
+            </div>
+            <div class="desc">
+                <p class="meta">
+                    <span class="cat"><a href="#">Events</a></span>
+                    <span class="date">20 March 2018</span>
+                    <span class="pos">By <a href="#">Rich</a></span>
+                </p>
+                <h2><a href="blog.html">Recipe for your site</a></h2>
+                <p>A small river named Duden flows by their place and supplies it with the necessary </p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="blog-entry">
+            <div class="blog-img">
+                <a href="blog.html"><img src="img/abricot/blog-6.jpg" class="img-responsive" alt="html5 bootstrap template"></a>
+            </div>
+            <div class="desc">
+                <p class="meta">
+                    <span class="cat"><a href="#">Events</a></span>
+                    <span class="date">20 March 2018</span>
+                    <span class="pos">By <a href="#">Rich</a></span>
+                </p>
+                <h2><a href="blog.html">Recipe for your site</a></h2>
+                <p>A small river named Duden flows by their place and supplies it with the necessary </p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row row-pb-md">
+    <div class="col-md-8">
+        <div class="blog-entry">
+            <div class="blog-img">
+                <div class="video colorlib-video" style="background-image: url(img/abricot/blog-8.jpg);">
+                    <a href="https://vimeo.com/channels/staffpicks/93951774" class="popup-vimeo"><i class="icon-play"></i></a>
+                    <div class="overlay"></div>
+                </div>
+            </div>
+            <div class="desc">
+                <p class="meta">
+                    <span class="cat"><a href="#">Watch</a></span>
+                    <span class="date">20 March 2018</span>
+                    <span class="pos">By <a href="#">Rich</a></span>
+                </p>
+                <h2><a href="blog.html">Watch our video how to make website</a></h2>
+                <p>A small river named Duden flows by their place and supplies it with the necessary </p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="blog-entry">
+            <div class="blog-img">
+                <a href="blog.html"><img src="img/abricot/blog-7.jpg" class="img-responsive" alt="html5 bootstrap template"></a>
+            </div>
+            <div class="desc">
+                <p class="meta">
+                    <span class="cat"><a href="#">Events</a></span>
+                    <span class="date">20 March 2018</span>
+                    <span class="pos">By <a href="#">Rich</a></span>
+                </p>
+                <h2><a href="blog.html">Recipe for your site</a></h2>
+                <p>A small river named Duden flows by their place and supplies it with the necessary </p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row row-pb-md">
+    <div class="col-md-4">
+        <div class="blog-entry">
+            <div class="blog-img">
+                <a href="blog.html"><img src="img/abricot/blog-9.jpg" class="img-responsive" alt="html5 bootstrap template"></a>
+            </div>
+            <div class="desc">
+                <p class="meta">
+                    <span class="cat"><a href="#">Events</a></span>
+                    <span class="date">20 March 2018</span>
+                    <span class="pos">By <a href="#">Rich</a></span>
+                </p>
+                <h2><a href="blog.html">Recipe for your site</a></h2>
+                <p>A small river named Duden flows by their place and supplies it with the necessary </p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="blog-entry">
+            <div class="blog-img">
+                <a href="blog.html"><img src="img/abricot/blog-10.jpg" class="img-responsive" alt="html5 bootstrap template"></a>
+            </div>
+            <div class="desc">
+                <p class="meta">
+                    <span class="cat"><a href="#">Events</a></span>
+                    <span class="date">20 March 2018</span>
+                    <span class="pos">By <a href="#">Rich</a></span>
+                </p>
+                <h2><a href="blog.html">Recipe for your site</a></h2>
+                <p>A small river named Duden flows by their place and supplies it with the necessary </p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="blog-entry">
+            <div class="blog-img">
+                <a href="blog.html"><img src="img/abricot/blog-11.jpg" class="img-responsive" alt="html5 bootstrap template"></a>
+            </div>
+            <div class="desc">
+                <p class="meta">
+                    <span class="cat"><a href="#">Events</a></span>
+                    <span class="date">20 March 2018</span>
+                    <span class="pos">By <a href="#">Rich</a></span>
+                </p>
+                <h2><a href="blog.html">Recipe for your site</a></h2>
+                <p>A small river named Duden flows by their place and supplies it with the necessary </p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row row-pb-md">
+    <div class="col-md-4">
+        <div class="blog-entry">
+            <div class="blog-img">
+                <a href="blog.html"><img src="img/abricot/blog-12.jpg" class="img-responsive" alt="html5 bootstrap template"></a>
+            </div>
+            <div class="desc">
+                <p class="meta">
+                    <span class="cat"><a href="#">Events</a></span>
+                    <span class="date">20 March 2018</span>
+                    <span class="pos">By <a href="#">Rich</a></span>
+                </p>
+                <h2><a href="blog.html">Recipe for your site</a></h2>
+                <p>A small river named Duden flows by their place and supplies it with the necessary </p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-8">
+        <div class="blog-entry">
+            <div class="blog-img">
+                <a href="blog.html"><img src="img/abricot/blog-13.jpg" class="img-responsive" alt="html5 bootstrap template"></a>
+            </div>
+            <div class="desc">
+                <p class="meta">
+                    <span class="cat"><a href="#">Events</a></span>
+                    <span class="date">20 March 2018</span>
+                    <span class="pos">By <a href="#">Rich</a></span>
+                </p>
+                <h2><a href="blog.html">Recipe for your site</a></h2>
+                <p>A small river named Duden flows by their place and supplies it with the necessary </p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12 text-center">
+        <ul class="pagination">
+            <li class="disabled"><a href="#">&laquo;</a></li>
+            <li class="active"><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">&raquo;</a></li>
+        </ul>
+    </div>
+</div>
+@stop
