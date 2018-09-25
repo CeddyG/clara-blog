d3819af8462b60620910881c8fd395fe7e09485e
CeddyG <ceddyg@free.fr>
2018-09-25 15:52:03 +0200

    -Fix Session with api

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,6 +41,7 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             'bindings',
             \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Session\Middleware\StartSession::class,
         ],
     ];
 
